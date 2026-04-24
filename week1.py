12bb32c55bd7c73bb0614480dd256c1259849b83
memonY49 <memonY49@gmail.com>
2026-04-24 20:54:39 +0500

    update

---
+++ week1.py
@@ -0,0 +1,136 @@
+"""
+    Data types:
+        integer: int
+        float: float
+        string: str
+        boolean: True,False
+    variables:
+        A container to store some data that can be changed.
+    How to ceate a variable:
+        syntax:
+            name_of_the_variable = value_of_the_variable
+
+    Naming Roles:
+        1. The name can not be started with any digit.
+        2. The name could not contains any white spaces in between.
+        3. The name could not be reserved words
+        4. Name could not containe any symbol except "_"
+
+
+x = 5
+
+print(x)
+
+x = 'ali'
+
+print(x)
+"""
+
+"""
+x = 5
+y = 6
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+"""
+
+"""
+Comparession operators:
+    1. less than: <
+    2. Greater than: >
+    3. less than or equals: <=
+    4. Greater than or equals: >=
+    5. equals: ==
+    6. not equals: !=
+"""
+"""
+logical operators:
+    1. and
+    2. or
+    3. not
+
+x = 4
+y = 7
+
+
+
+print(x<y and x>=4)
+"""
+
+
+"""
+class tasks:
+    1. To convert temprature from 92f to c ((F − 32) × 5/9)
+    2. To convert lenght unit from 132cm to km (km=cm/100000)
+    3. To convert weight unit from 1680mg to pounds (p = mg x 0.00000220462)
+
+
+
+f = 92
+c = (f - 32) * 5/9  # f to c conversion
+cm = 132
+km = cm/100000      # cm to km
+mg = 1680
+p  = mg * 0.00000220462  # mg to pounds
+
+print(c)
+print(km)
+print(p)
+
+"""
+
+"""
+How to take user input in python.
+syntax:
+    input()      # input func takes string as an input
+"""
+
+#name = input("Enter your Name: ")
+#print(name)
+
+
+"""
+Type casting:
+    1. implicit
+    2. explicit
+
+"""
+
+
+
+f = int(input("Enter your f value: "))
+c = (f - 32) * 5/9  # f to c conversion
+print(c)
+
+
+cm = int(input("Enter your cm value: "))
+km = cm/100000      # cm to km
+print(km)
+
+mg = int(input("Enter your mg value: "))
+p  = mg * 0.00000220462  # mg to pounds
+print(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
