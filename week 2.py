2791325acbb1b296f7e6a94ed0bd65e8294a7725
memony49 <memony49@gmail.com>
2026-05-08 21:02:15 +0500

    Update

--- week 2.py
+++ week 2.py
@@ -26,7 +26,7 @@ else:
 # compare if age>15 and <25 the person is called collage passed.
 # if age>25 and < 30 the person is uni passed
 # if age> 30 the person is in masters.
-
+"""
 
 age = 30
 if age<15:
@@ -37,14 +37,119 @@ if age>=25 and age<30:
     print("the person is uni passed")
 if age>=30:
     print("the person is in masters.")
-
+"""
 
 # Task 1:
 # step 1: Create at least 5 variable(name,fname,phone,email and password)
-# step 2: Create two extra variables for useremail and userpass taken as an input
+# step 2: Create two extra variables for useremail and userpass take as an input
 # from the user
 # step 3: Match email == useremail and password == userpass
 # step 4: If matched print all details else "email or password was incorrect"
+"""
+name = "yasir"
+fname = "nawaz"
+phone = "0300000000"
+email = "abc@gmail.com"
+password = "abc123"
+
+useremail = input("Enter your email: ")
+userpass = input("Enter your password: ")
+
+if email == useremail and password == userpass:
+    print(name)
+    print(fname)
+    print(phone)
+    print(email)
+else:
+    print("email or password was incorrect")
+"""
+
+
+"""
+x = 5
+y = 6
+
+if x < y:
+    print("less than")
+elif x > y:
+    print("greater than")
+else:
+    print("equals than")
+"""
+
+
+
+# Nasted if:
+#       if inside of an if
+"""
+x = 6
+y = 8
+
+if x<y:
+    if x>0:
+        print("x is in range of 0 - 8")
+"""
+
+"""
+name = "yasir"
+fname = "nawaz"
+phone = "0300000000"
+email = "abc@gmail.com"
+password = "abc123"
+
+useremail = input("Enter your email: ")
+userpass = input("Enter your password: ")
+
+if email == useremail:
+    if password == userpass:
+        print(name)
+        print(fname)
+        print(phone)
+        print(email)
+    else:
+        print("password is incorrect")
+else:
+    print("email is incorrect")
+"""
+
+
+#task :
+# take number variable from the user and check if the number is even than
+# print "even number" else print "odd number"
+
+
+number = int(input("Enter your number: "))
+if number%2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ week 3.py
@@ -0,0 +1,129 @@
+
+
+# Loops:
+#   repeating a block of code for multiple times at same place until
+#   the condition is true
+#   types of loops:
+#       1. for
+#       2. while
+#
+
+# for loop:
+#   syntax:
+#       for counter in range(lb, ub):
+#           body of loop
+
+
+#for counter in range(1,11):
+#    print(13,"X",counter,"=",counter*13)
+
+# while loop:
+#   syntax:
+#       intialize counter
+#       while(condition):
+#           body of while
+#           inc/dec
+
+#counter = 10
+#while counter >=1:
+#    print(counter)
+#    counter -= 1
+
+"""
+* * * *
+* * * *
+* * * *
+* * * *
+"""
+
+#for i in range(1,5):
+#    print("* "*4)
+
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+i = 1
+while i<=5:
+    print("* "*i)
+    i+=1
+    
+j = 4
+while j>=1:
+    print("* "*j)
+    j-=1
+
+
+for i in range(1,6):
+    print("* "*i)
+for j in range(4,0,-1):
+    print("* "*j)
+"""
+
+
+"""
+print parameters:
+    1. str|String|text : The string to be printed
+    2. sep             : To seperate two or more strings
+    3. end             : By default it ends the line and
+                         starts a new line for next print
+"""
+
+#print("Hello",end=" ")
+#print("world")
+
+#print("Hello","World",sep = "_")
+
+
+"""
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+"""
+
+
+for i in range(1,6):
+    print("-"*(5-i),end="")
+    print("* "*i)
+    
+for i in range(1,6):
+    print("-"*i,end="")
+    print("* "*(5-i))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
