cf82cdf29678c2b62451e7c565cdde12d0592888
memony49 <memony49@gmail.com>
2026-06-16 22:04:25 +0500

    Update

--- OS_Module.py
+++ OS_Module.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 
 # print(os.environ['yasir'])
 # print(os.environ['PATH'])
@@ -24,10 +25,51 @@ import os
 
 # os.system("rmdir data")
 
-if os.path.exists("Data/A/test0.txt"):
-    os.remove("Data/A/test0.txt")
-    print("File removed!!")
-else:
-    print("file does not exists")
+# if os.path.exists("Data/A/test0.txt"):
+#     os.remove("Data/A/test0.txt")
+#     print("File removed!!")
+# else:
+#     print("file does not exists")
 
 
+# try:
+#     os.remove("Data/A/test0.txt")
+#     print("File removed!!")
+# except FileNotFoundError:
+#     print("File not found")
+
+
+# dir = "D:\\"
+# folder = "salani"
+# subfolder = "Batch 5"
+# file = "data.txt"
+# print(f"{dir}{folder}/{subfolder}/{file}")
+# filepath = os.path.join(dir,folder,subfolder,file)
+# print(os.path.split(filepath))
+# print(os.path.splitext("filehandling.py"))
+# print(os.listdir())
+
+# listofdir = os.listdir()
+# listofpy = []
+# for file in listofdir:
+#     exten = os.path.splitext(file)[1]
+#     if exten == ".py":
+#         listofpy.append(file)
+
+
+# print(listofpy)
+
+
+# path = "D:\\salani\\Batch 5\\Data\\B"
+# print(os.path.dirname(path))
+
+
+path =pathlib.Path("D:\\salani\\Batch 5\\Data\\B\\test1.txt")
+
+# print(path.home())
+# print(path.is_file())
+# print(path.suffix)
+# print(path.name)
+file = path.open()
+print(file.read())
+file.close()
